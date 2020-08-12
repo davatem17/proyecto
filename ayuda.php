14a17603e4a42cd187b6367fbdc6f9cace56ef54
davatem17 <perezd625@gmail.com>
2020-08-12 13:20:40 -0500

    Ayuda con tablas

--- ayuda.php
+++ ayuda.php
@@ -8,5 +8,13 @@
 <body>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates magnam aperiam nulla, cupiditate, quas ratione modi aliquid incidunt quae delectus, libero veritatis. Corrupti non veritatis voluptas deleniti dolore doloribus quod.</p>    
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus nam laudantium quis fugiat nisi laborum facilis accusantium quibusdam, rerum necessitatibus maxime eaque deleniti commodi earum possimus alias iusto ut dolores.</p>
+    <table>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
 </body>
 </html>
